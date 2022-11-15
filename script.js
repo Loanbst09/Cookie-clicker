728df6d79bf57a41761aed2445cabac8bed0215d
Loanbst09 <95340906+Loanbst09@users.noreply.github.com>
2022-11-15 15:45:58 +0100

    JS

---
+++ script.js
@@ -0,0 +1,115 @@
+let gymnaste = 0;
+let nb_gymnaste_per_click = 1
+let multiplicateur = 0
+let multiplicateur_cost = 10
+let entraineur = 0
+let entraineur_cost = 200
+let bonus = 0
+let bonus_cost = 1000
+document.querySelector (".multiplicateur_cost").innerHTML = multiplicateur_cost
+document.querySelector (".entraineur_cost").innerHTML = entraineur_cost
+document.querySelector (".bonus_cost").innerHTML = bonus_cost
+
+
+
+// function recuperer_images(){
+//     let images = [
+//         {url : "images/gymnaste4.png", min:160 },
+//         {url : "images/gymnaste3.png", min:120 },
+//         {url : "images/gymnaste2.png", min:10 },
+//         {url : "images/olympic-gymnastic-png.png", min:0 },
+//     ];
+
+//     result = images.find (element => element.min <= gymnaste)
+//     return result.url
+// }
+
+
+// ---multiplicateur---//
+
+
+document.querySelector(".image").addEventListener("click", mettreAJourMonResult)
+function mettreAJourMonResult(){
+    gymnaste = gymnaste + nb_gymnaste_per_click
+    document.querySelector(".result").innerHTML = gymnaste
+     document.getElementById ('gymage').src =recuperer_images();
+}
+
+
+
+document.querySelector(".médailles-b-img").addEventListener("click", augmenterMultiplicateur)
+function augmenterMultiplicateur(){
+    if (gymnaste>=multiplicateur_cost) {
+    nb_gymnaste_per_click = nb_gymnaste_per_click *2
+    gymnaste = gymnaste - multiplicateur_cost
+    multiplicateur_cost = multiplicateur_cost *2
+    multiplicateur = multiplicateur + 1
+    document.querySelector (".multiplicateur").innerHTML = multiplicateur
+    document.querySelector (".multiplicateur_cost").innerHTML = multiplicateur_cost
+    document.querySelector(".result").innerHTML = gymnaste
+    }
+    else{
+        alert ("Vous n'avez pas assez de gymnastes! \nCliquez encore.")
+    }
+}
+ 
+
+// ---autoclicker---//
+
+
+let click = function(){
+    gymnaste = gymnaste + entraineur
+    document.querySelector (".entraineur").innerHTML = entraineur
+    document.querySelector (".entraineur_cost").innerHTML = entraineur_cost
+    document.querySelector(".result").innerHTML = gymnaste
+}
+
+document.querySelector(".entraineur-img").addEventListener("click", mettreAJourentraineur)
+function mettreAJourentraineur(){
+    if (gymnaste>=entraineur_cost) {
+    gymnaste = gymnaste - entraineur_cost
+    entraineur_cost = entraineur_cost *2
+    entraineur = entraineur + 1
+    setInterval(click, 1000)
+    }
+    else{
+        alert ("Vous n'avez pas assez de gymnastes! \nCliquez encore.")
+    }
+}
+
+// ---bonus---//
+
+
+document.querySelector("#bonus").addEventListener("click", augmenterbonus)
+function augmenterbonus(){
+    if (gymnaste>=bonus_cost) {
+    nb_gymnaste_per_click = nb_gymnaste_per_click *(1+200/100)
+    gymnaste = gymnaste - bonus_cost
+    bonus = bonus + 1
+    document.querySelector(".result").innerHTML = gymnaste
+    }
+    else{
+        alert ("Vous n'avez pas assez de gymnastes! \nCliquez encore.")
+    }
+}
+
+// Texte aléatoire
+
+function getPhrase(){
+    let listePhrases = [
+        "Tu viens de tomber de la poutre", 
+        "Mince tu viens de dépasser la limite autorisé",
+        "Tu pense finir sur le podium?",
+
+
+    ];
+
+    document.getElementById("phrase").innerHTML=listePhrases[Math.trunc(Math.random() * listePhrases.length)];
+}
+
+setInterval(getPhrase, 3000);
+
+
+
+
+
